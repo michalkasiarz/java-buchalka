d7a1f0cafc0e8d8c64d52f73b018b28409ddf00d
michajlyk <michal.kasiarz@post.com>
2018-08-05 16:14:37 +0200

    Char and boolean exercises

---
+++ CharAndBoolean.java
@@ -0,0 +1,16 @@
+package com.michal;
+
+public class CharAndBoolean {
+
+    public static void main(String[] args) {
+	    char myChar = '\u00A7';
+        System.out.println(myChar);
+
+        boolean myBoolean = true;
+        boolean isMale = true;
+
+        // Challenge
+        char registeredSymbol = '\u00AE';
+        System.out.println(registeredSymbol);
+    }
+}
