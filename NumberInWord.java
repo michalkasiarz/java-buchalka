7f99f5d81c9ce800c89d2ad161a0b116bb73ff87
michajlyk <michal.kasiarz@post.com>
2018-08-06 11:25:21 +0200

    Checks if the given value is equal to 1-9 and print it

---
+++ NumberInWord.java
@@ -0,0 +1,48 @@
+public class NumberInWord {
+
+    public static void main(String[] args) {
+        printNumberInWord(2);
+        printNumberInWord(-5);
+        printNumberInWord(9);
+        printNumberInWord(22);
+    }
+
+
+    public static void printNumberInWord(int number) {
+        switch (number) {
+            case 0:
+                System.out.println("ZERO");
+                break;
+            case 1:
+                System.out.println("ONE");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+            case 6:
+                System.out.println("SIX");
+                break;
+            case 7:
+                System.out.println("SEVEN");
+                break;
+            case 8:
+                System.out.println("EIGHT");
+                break;
+            case 9:
+                System.out.println("NINE");
+                break;
+            default:
+                System.out.println("OTHER");
+                break;
+        }
+    }
+}
