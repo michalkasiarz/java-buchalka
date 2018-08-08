d9f72ba76748c7b4580a1aa494947e66a708f651
michajlyk <michal.kasiarz@post.com>
2018-08-08 08:08:01 +0200

    checks whether both given numbers share any digit

---
+++ SharedDigit.java
@@ -0,0 +1,30 @@
+public class SharedDigit {
+
+    public static void main(String[] args) {
+        System.out.println(hasSharedDigit(12, 23));
+        System.out.println(hasSharedDigit(9, 90));
+        System.out.println(hasSharedDigit(15, 55));
+        System.out.println(hasSharedDigit(12, 93));
+        System.out.println(hasSharedDigit(77, 17));
+    }
+
+
+    public static boolean hasSharedDigit(int firstNumber, int secondNumber) {
+        if ((firstNumber >= 10 && firstNumber <= 99) && (secondNumber >= 10 && secondNumber <= 99)) {
+            int firstNumberLastDigit = 0;
+            int secondNumberLastDigit = 0;
+            int firstNumberFirstDigit = 0;
+            int secondNumberFirstDigit = 0;
+            firstNumberLastDigit = firstNumber % 10;
+            secondNumberLastDigit = secondNumber % 10;
+            firstNumber /= 10;
+            secondNumber /= 10;
+            firstNumberFirstDigit = firstNumber;
+            secondNumberFirstDigit = secondNumber;
+            System.out.println("Is there any shared digit in both given numbers?");
+            return ((firstNumberFirstDigit == secondNumberFirstDigit) || (firstNumberFirstDigit == secondNumberLastDigit) || (firstNumberLastDigit == secondNumberFirstDigit) || (firstNumberLastDigit == secondNumberLastDigit));
+        }
+        System.out.println("Invalid input.");
+        return false;
+    }
+}
