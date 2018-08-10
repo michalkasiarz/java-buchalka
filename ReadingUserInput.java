aac1fecfa9c1a256c38a195ed87846866633352f
michajlyk <michal.kasiarz@post.com>
2018-08-10 22:03:38 +0200

    added handling wrong input

--- ReadingUserInput.java
+++ ReadingUserInput.java
@@ -3,17 +3,27 @@ import java.util.Scanner;
 public class ReadingUserInput {
 
     public static void main(String[] args) {
-	    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); // handle nextline character (enter key)
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
+        boolean hasNextInt = scanner.hasNextInt();
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle nextline character (enter key)
 
-        int age = 2018 - yearOfBirth;
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
 
-        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            int age = 2018 - yearOfBirth;
+
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.");
+        }
         scanner.close();
     }
 }
