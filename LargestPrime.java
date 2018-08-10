2a155b181d41c292d647a96c862a2570c044397d
michajlyk <michal.kasiarz@post.com>
2018-08-10 15:59:33 +0200

    calculates the largest prime of a given number

---
+++ LargestPrime.java
@@ -0,0 +1,23 @@
+public class LargestPrime {
+
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(-1));
+    }
+
+    public static int getLargestPrime(int number) {
+        if (number >= 2) {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    number /= i;
+                    i--;
+                }
+            }
+            return number;
+        }
+        return -1;
+    }
+}
