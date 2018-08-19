1954d5647dbb14483c9cc0ef36aaf01ed2d0010f
michajlyk <michal.kasiarz@post.com>
2018-08-19 16:58:07 +0200

    simple app for managing a grocery list

---
+++ arrays-lists-autoboxing-unboxing/GroceryApp/GroceryApp.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class GroceryApp {
+    private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
+
+
+    public static void main(String[] args) {
+        boolean quit = false;
+        int choice = 0;
+        printInstructions();
+        while (!quit) {
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    groceryList.printGroceryList();
+                    break;
+                case 2:
+                    addItem();
+                    break;
+                case 3:
+                    modifyItem();
+                    break;
+                case 4:
+                    removeItem();
+                    break;
+                case 5:
+                    searchForItem();
+                    break;
+                case 6:
+                    quit = true;
+                    break;
+            }
+        }
+    }
+
+    public static void printInstructions() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 to print the choice options");
+        System.out.println("\t 1 to print the grocery list");
+        System.out.println("\t 2 to add an item to the list");
+        System.out.println("\t 3 to modify an item in the list");
+        System.out.println("\t 4 to remove an item from the list");
+        System.out.println("\t 5 to search for an item in the list");
+        System.out.println("\t 6 to quit the application");
+    }
+
+    public static void addItem() {
+        System.out.print("Please enter the grocery item: ");
+        groceryList.addGroceryItem(scanner.nextLine());
+    }
+
+    public static void modifyItem() {
+        System.out.print("Enter item number:");
+        int itemNo = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter replacement item");
+        String newItem = scanner.nextLine();
+        groceryList.modifyGroceryItem(itemNo-1, newItem);
+    }
+
+    public static void removeItem() {
+        System.out.print("Enter item number:");
+        int itemNo = scanner.nextInt();
+        scanner.nextLine();
+        groceryList.removeGroceryItem(itemNo-1);
+    }
+
+    public static void searchForItem() {
+        System.out.println("Item to search for:");
+        String searchItem = scanner.nextLine();
+        if(groceryList.findItem(searchItem) != null) {
+            System.out.println("Found " + searchItem + " in our grocery list");
+        } else {
+            System.out.println(searchItem + " is not in the shopping list");
+        }
+    }
+
+}
