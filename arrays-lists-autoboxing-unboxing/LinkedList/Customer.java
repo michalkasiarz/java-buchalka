35878efe276c7e5dcc0be6a77171bc6aa0a7c624
michalkasiarz <michal.kasiarz@post.com>
2018-08-25 09:49:51 +0200

    LinkedList first shot

---
+++ arrays-lists-autoboxing-unboxing/LinkedList/Customer.java
@@ -0,0 +1,25 @@
+public class Customer {
+    private String name;
+    private double balance;
+
+    public Customer(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
---
+++ arrays-lists-autoboxing-unboxing/LinkedList/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Customer customer = new Customer("Michal", 54.96);
+        Customer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(12.18);
+        System.out.println("Balance for customer: " + customer.getName() + " is " + customer.getBalance());
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        for(int i=0; i<intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        intList.add(1, 2);
+
+        for(int i=0; i<intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+    }
+}
