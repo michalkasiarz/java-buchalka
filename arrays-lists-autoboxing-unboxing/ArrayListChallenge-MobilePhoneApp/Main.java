964149a980805ad3babdb6767cacc916427be36e
michalkasiarz <michal.kasiarz@post.com>
2018-09-06 11:09:26 +0200

    updating contact added

--- arrays-lists-autoboxing-unboxing/ArrayListChallenge-MobilePhoneApp/Main.java
+++ arrays-lists-autoboxing-unboxing/ArrayListChallenge-MobilePhoneApp/Main.java
@@ -1,12 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-        MobilePhone myPhone = new MobilePhone();
+        MobilePhone myPhone = new MobilePhone("900500300");
         myPhone.addContact("Janusz", "123123");
         myPhone.addContact("Zachar", "6654332");
         myPhone.addContact("Jędrzej", "74322");
 
+        System.out.println();
+
         myPhone.printContacts();
+
+        System.out.println();
+
+        myPhone.updateContact("Zachar", "Piotrek", "123");
+
+        System.out.println();
+
+        myPhone.printContacts();
+
+        System.out.println();
+
+        myPhone.updateContact("Paweł", "Gaweł", "12412111");
+
 
     }
 }
--- arrays-lists-autoboxing-unboxing/ArrayListChallenge-MobilePhoneApp/MobilePhone.java
+++ arrays-lists-autoboxing-unboxing/ArrayListChallenge-MobilePhoneApp/MobilePhone.java
@@ -5,14 +5,41 @@ public class MobilePhone {
     public String myNumber;
     public List<Contact> myContacts = new ArrayList<>();
 
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = myContacts;
+    }
+
     public void printContacts() {
+        System.out.println("Contact list of " + myNumber + ":");
         for (int i = 0; i < myContacts.size(); i++) {
             System.out.println(myContacts.get(i).getName());
         }
     }
 
-    public void addContact(String name, String myNumber) {
-        Contact contact = new Contact(name, myNumber);
+    public void addContact(String name, String number) {
+        Contact contact = new Contact(name, number);
         myContacts.add(contact);
+        System.out.println("New contact added.");
+    }
+
+    public void updateContact(String oldName, String newName, String number) {
+        int queryResult = queryContact(oldName);
+        if(queryResult > 0) {
+            Contact updatedContact = new Contact(newName, number);
+            myContacts.set(queryResult, updatedContact);
+            System.out.println("Contact " + oldName + " has been replaced with " + newName + " (phone: " + number + ").");
+        } else {
+            System.out.println("Operation error. Contact " + oldName + " was not found on your contact list, so contact was not updated.");
+        }
+    }
+
+    public int queryContact(String contactName) {
+        for (int i = 0; i < myContacts.size(); i++) {
+            if(contactName.equals(myContacts.get(i).getName())) {
+                return i;
+            }
+        }
+        return 0;
     }
 }
