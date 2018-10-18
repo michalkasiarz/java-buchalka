2671b691a26a1d8a31c5e3a011ab9a0bfbe9593a
michalkasiarz <michal.kasiarz@post.com>
2018-10-18 13:45:51 +0200

    gearbox class added (with inner class named gear)

---
+++ inner-abstract-classes-interfaces/inner-classes/Gearbox.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Gearbox {
+    private ArrayList<Gear> gears;
+    private int maxGears;
+    private int currentGear = 0;
+
+    public Gearbox(int maxGears) {
+        this.maxGears = maxGears;
+        this.gears = new ArrayList<>();
+        Gear neutral = new Gear(0, 0.0);
+        this.gears.add(neutral);
+    }
+
+    public class Gear {
+        private int gearNumber;
+        private double ratio;
+
+        public Gear(int gearNumber, double ratio) {
+            this.gearNumber = gearNumber;
+            this.ratio = ratio;
+        }
+
+        public double driveSpeed(int revs) {
+            return revs * (this.ratio);
+        }
+    }
+
+}
