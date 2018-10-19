a8ba727a0b82e42f567f4a602059c2d9b034c379
michalkasiarz <michal.kasiarz@post.com>
2018-10-19 12:33:42 +0200

    main class added, abstract classes example done

---
+++ inner-abstract-classes-interfaces/abstract-classes/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Dog dog = new Dog("Odys");
+        dog.breathe();
+        dog.eat();
+
+        Parrot parrot = new Parrot("Polly");
+        parrot.breathe();
+        parrot.eat();
+        parrot.fly();
+
+        Penguin penguin = new Penguin("Emperor");
+        penguin.fly();
+    }
+}
