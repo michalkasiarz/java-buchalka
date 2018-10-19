ae196868676a9437591639139d4774435d20ef56
michalkasiarz <michal.kasiarz@post.com>
2018-10-19 07:18:36 +0200

    gearbox class updated

--- inner-abstract-classes-interfaces/inner-classes/Gearbox.java
+++ inner-abstract-classes-interfaces/inner-classes/Gearbox.java
@@ -4,6 +4,7 @@ public class Gearbox {
     private ArrayList<Gear> gears;
     private int maxGears;
     private int currentGear = 0;
+    private boolean clutchIsIn;
 
     public Gearbox(int maxGears) {
         this.maxGears = maxGears;
@@ -12,6 +13,29 @@ public class Gearbox {
         this.gears.add(neutral);
     }
 
+    public void operateClutch(boolean in) {
+        this.clutchIsIn = in;
+    }
+
+    public void addGear(int newGear) {
+        if ((newGear >= 0) && newGear < this.gears.size() && this.clutchIsIn) {
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected.");
+        } else {
+            System.out.println("Grind!");
+            this.currentGear = 0;
+        }
+    }
+
+    public double wheelSpeed(int revs) {
+        if (clutchIsIn) {
+            System.out.println("Scream!");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
+    }
+
+
     public class Gear {
         private int gearNumber;
         private double ratio;
@@ -19,6 +43,10 @@ public class Gearbox {
         public Gear(int gearNumber, double ratio) {
             this.gearNumber = gearNumber;
             this.ratio = ratio;
+        }
+
+        public double getRatio() {
+            return ratio;
         }
 
         public double driveSpeed(int revs) {
--- inner-abstract-classes-interfaces/inner-classes/Main.java
+++ inner-abstract-classes-interfaces/inner-classes/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Gearbox mcLaren = new Gearbox(6);
+	    Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+        System.out.println(first.driveSpeed(1000));
     }
 }
