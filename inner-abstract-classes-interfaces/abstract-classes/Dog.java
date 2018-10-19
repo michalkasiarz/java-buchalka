ae7cf585c08b2b65bb53dd98f13ad1ab8018ba89
michalkasiarz <michal.kasiarz@post.com>
2018-10-19 12:18:57 +0200

    Dog class added (extends Animal class)

---
+++ inner-abstract-classes-interfaces/abstract-classes/Dog.java
@@ -0,0 +1,20 @@
+public class Dog extends Animal {
+
+    // calling constructor from parent class
+    public Dog(String name) {
+        super(name);
+    }
+
+    // overriding eat() method
+    @Override
+    public void eat() {
+        System.out.println(getName() + " chews");
+    }
+
+    // overriding breathe() method
+    @Override
+    public void breathe() {
+        System.out.println("Breathe in, breathe out. ");
+
+    }
+}
